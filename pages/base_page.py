0a6cd8439391d23a41c092b5e4638ad6c338dd24
Ilnur Abdullin <ilnur6005@gmail.com>
2024-10-18 17:35:42 +0400

    Moved funcs go_to_login_page, should_be_login_link from main_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,8 @@
 from selenium.common import NoSuchElementException, NoAlertPresentException, TimeoutException
-from math import log, sin
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from .locators import BasePageLocators
+from math import log, sin
 
 
 class BasePage:
@@ -9,6 +10,14 @@ class BasePage:
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), \
+            "Login link is not presented"
 
     def open(self):
         self.browser.get(self.url)
