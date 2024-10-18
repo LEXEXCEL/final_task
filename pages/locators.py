eeed0f424def7de5a2d07061ac6d13d0b403aca1
Ilnur Abdullin <ilnur6005@gmail.com>
2024-10-18 15:45:20 +0400

    Added var SUCCESS_MESSAGE in ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -10,9 +10,10 @@ class LoginPageLocators:
     REGISTRATION_FORM_SELECTOR = (By.CSS_SELECTOR, "#register_form")
 
 
-class CartPage:
+class ProductPageLocators:
     ADD_TO_CART_BUTTON_SELECTOR = (By.CSS_SELECTOR, ".btn-add-to-basket")
     ITEM_NAME = (By.CSS_SELECTOR, ".product_main h1")
     ITEM_NAME_IN_CART = (By.CSS_SELECTOR, ".alert-noicon:nth-child(1) strong")
     ITEM_PRICE = (By.CSS_SELECTOR, ".product_main [class='price_color']")
     ITEM_PRICE_IN_CART = (By.CSS_SELECTOR, ".alert-noicon:nth-child(3) strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1)")
