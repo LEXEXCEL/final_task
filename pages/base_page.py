8eb5edfcafb131623b8b6919fb421c12b0eb7e14
Ilnur Abdullin <ilnur6005@gmail.com>
2024-10-19 13:28:56 +0400

    Added funcs: should_not_be_items_in_basket, is_text_empty_basket_presented

--- pages/base_page.py
+++ pages/base_page.py
@@ -11,16 +11,12 @@ class BasePage:
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def go_to_login_page(self):
-        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
-        login_link.click()
-
-    def should_be_login_link(self):
-        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), \
-            "Login link is not presented"
-
-    def open(self):
-        self.browser.get(self.url)
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
     def is_not_element_present(self, how, what, timeout=4):
         try:
@@ -37,12 +33,20 @@ class BasePage:
             return False
         return True
 
-    def is_element_present(self, how, what):
-        try:
-            self.browser.find_element(how, what)
-        except NoSuchElementException:
-            return False
-        return True
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def go_to_basket_page(self):
+        basket_page = self.browser.find_element(*BasePageLocators.BASKET_LINK)
+        basket_page.click()
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), \
+            "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
