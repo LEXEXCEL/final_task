582a2d8059daee64ab9ab932226da17cc40fe169
Ilnur Abdullin <ilnur6005@gmail.com>
2024-10-19 23:13:23 +0400

    Added class TestLoginFromMainPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,28 +1,32 @@
+import pytest
 from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com"
 
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.go_to_login_page()
+        login_page.should_be_login_page()
 
 
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com"
 
-    page = MainPage(browser, link)
-    page.open()
+        page = MainPage(browser, link)
+        page.open()
 
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.go_to_basket_page()
 
-    basket_page.should_not_be_items_in_basket()
-    basket_page.is_text_empty_basket_presented()
+        basket_page.should_not_be_items_in_basket()
+        basket_page.is_text_empty_basket_presented()
     
